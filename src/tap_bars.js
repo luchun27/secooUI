92e66a90a192ebea005b8d18bcd52123217fc6e3
HuZai <zhenghu1989415@163.com>
2016-09-14 16:26:15 +0800

    tap update base scss edit

--- src/tap_bars.js
+++ src/tap_bars.js
@@ -1,3 +1,28 @@
 //顶部导航
 require('styles/tap_bars/tap.scss');
+(function(){
+    let $seach_label= $('#search_box .seach_label');
+    let $secoo_logo= $('.search_all .secoo_logo');
+    let $search_box= $('.search_all .search_box');
+    let $search_close= $('.search_all .close');
+    let $search_input= $('#search_input');
+    $('#search_box').on('focus','#search_input',function(){
+        $seach_label.hide();
+        $secoo_logo.hide();
+        $search_box.removeClass('seach_box_w');
+    }).on('blur','#search_input',function(){
+        console.log('blur');
+        $search_box.addClass('seach_box_w');
+        $secoo_logo.show();
+        $seach_label.show();
+    }).on('input','#search_input',function(){
+        $search_close.removeClass('hide');
+    }).on('touchend','.close',function(){
+        $search_input.val('');
+        $search_close.addClass('hide');
+    }).on('touchend','.cannel',function(){
+        $search_input.val('');
+    });
+})();
+
 
